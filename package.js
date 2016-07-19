c0ea9264885a5a143c8851262802ed54c9068ee3
Yannic Kilcher <yk@users.noreply.github.com>
2016-07-19 16:13:52 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'typ:ldapjs',
+  name: 'yk:ldapjs',
   version: '0.7.3',
   summary: 'Meteor package wrapper for the ldapjs Npm module https://www.npmjs.com/package/ldapjs',
   git: 'https://github.com/yk/meteor-ldapjs',
